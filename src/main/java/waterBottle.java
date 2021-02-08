c13f3cba116320b0cde6327f5d613ba0a456cfa4
Juan <jpalenzuela@outlook.com>
2021-02-08 23:54:14 +0000

    MVP Completed

--- src/main/java/waterBottle.java
+++ src/main/java/waterBottle.java
@@ -1,4 +1,21 @@
 public class waterBottle {
 
+    public int content;
 
+    public waterBottle(){
+        this.content = 100;
+    }
+
+
+    public int drink() {
+        return this.content -= 10;
+    }
+
+    public int empty() {
+        return this.content = 0;
+    }
+
+    public int fill() {
+        return this.content = 100;
+    }
 }
--- src/test/java/waterBottleTest.java
+++ src/test/java/waterBottleTest.java
@@ -1,6 +1,33 @@
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class waterBottleTest {
 
+    waterBottle waterBottle;
+    @Before
+    public void before(){
+        waterBottle = new waterBottle();
+    }
+
     @Test
+    public void startsAt100(){
+        assertEquals(100, this.waterBottle.content);
+    }
+
+    @Test
+    public void drink(){
+        assertEquals(90, this.waterBottle.drink());
+    }
+
+    @Test
+    public void empty(){
+        assertEquals(0, this.waterBottle.empty());
+    }
+
+    @Test
+    public void fill(){
+        assertEquals(100, this.waterBottle.fill());
+    }
 }
